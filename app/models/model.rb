3d4b78002cce9ab92f5bbada8a1e94c6b30b5ae4
Dudisek <peta.duda@gmail.com>
2015-12-31 09:59:15 +0800

    hello delete

--- app/models/model.rb
+++ app/models/model.rb
@@ -3,5 +3,4 @@ class Model < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         hello
 end
