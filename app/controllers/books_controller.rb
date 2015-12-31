e770ab2c532f1cef6dc26848b68f4df4f65b0239
Sam <samkahchiin@gmail.com>
2015-12-31 12:11:41 +0800

    create form for book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,22 @@
 class BooksController < ApplicationController
 
   def new
-    
+    @book = Book.new
   end
+
+  def create
+		@book = Book.new(book_params)
+		@book.save!
+		redirect_to @book
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+	private
+	def listing_params
+		params.require(:book).permit(:name, :price, :categories, :condition, :description, :prefered_location).merge(user_id: current_user.id)
+	end
 
 end
