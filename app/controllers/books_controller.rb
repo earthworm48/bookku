1402dab0b02f445457607cc72adfa3e6d3047e2b
Sam <samkahchiin@gmail.com>
2015-12-31 12:33:49 +0800

    checking the edit function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,21 @@ class BooksController < ApplicationController
   	@book = Book.find(params[:id])
   end
 
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+
+  	@book = Book.find(params[:id])
+		if @book.update(book_params)
+			byebug
+			redirect_to @book
+		else
+			render :edit
+		end
+  end
+
 	private
 	def book_params
 		params.require(:book).permit(:name, :price, :categories, :condition, :description, :prefered_location)
