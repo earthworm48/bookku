399cc32d6ed80d80e5e8dd16bbb433a8cc827768
Dudisek <peta.duda@gmail.com>
2015-12-31 12:55:21 +0800

    figaro changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151231032534) do
+ActiveRecord::Schema.define(version: 20151231043556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "book_transactions", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.string  "status"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
