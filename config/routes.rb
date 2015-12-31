a086c27f3bcf74dc560fd41322bb20b774fa3d07
Dudisek <peta.duda@gmail.com>
2015-12-31 11:30:17 +0800

    FB improvment

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
 
   resources :users, only: [:show]
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
