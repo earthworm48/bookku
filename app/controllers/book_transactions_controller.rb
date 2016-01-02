e684a4a9198af8ce48ff5ddf899394d5f2058067
Sam <samkahchiin@gmail.com>
2016-01-02 18:15:00 +0800

    points is resume if the user delete transaction

---
+++ app/controllers/book_transactions_controller.rb
@@ -0,0 +1,30 @@
+
+class BookTransactionsController < ApplicationController
+  
+  def create
+  	
+  	@book_transaction = current_user.book_transactions.new(book_transaction_params)
+    
+    if @book_transaction.save
+      @book_transaction.user.points -= @book_transaction.book.price
+      @book_transaction.user.save!
+    end
+    # byebug
+    redirect_to current_user
+  end
+
+  def destroy
+  	@book_transaction = BookTransaction.find(params[:id])
+  	
+    if @book_transaction.destroy
+      @book_transaction.user.points += @book_transaction.book.price    
+      @book_transaction.user.save!
+    end
+  	redirect_to current_user	
+  end
+
+  private
+  def book_transaction_params
+  	params.require(:book_transaction).permit(:book_id)
+  end
+end
