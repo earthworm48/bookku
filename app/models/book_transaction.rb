e9ba618bb451f772fd0054db11eacc71eb766ac2
Sam <samkahchiin@gmail.com>
2016-01-03 14:32:01 +0800

    validate in model try 1

--- app/models/book_transaction.rb
+++ app/models/book_transaction.rb
@@ -1,4 +1,13 @@
 class BookTransaction < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :book
+
+	before_save :point_validation
+
+	def point_validation
+		# byebug
+		if user.points < book.price
+			reject transaction
+		end
+	end
 end
