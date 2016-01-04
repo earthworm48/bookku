384f663577926a6537a759bc10176ba1b1058bd1
Sam <samkahchiin@gmail.com>
2016-01-04 18:23:53 +0800

    change the type of isbn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104100829) do
+ActiveRecord::Schema.define(version: 20160104102227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160104100829) do
     t.datetime "updated_at",        null: false
     t.integer  "user_id"
     t.integer  "isbn"
+    t.string   "image_url"
   end
 
   add_index "books", ["user_id"], name: "index_books_on_user_id", using: :btree
