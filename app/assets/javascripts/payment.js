19c1b4f2ce780df745c1095025a7ae04229fd827
Sam <samkahchiin@gmail.com>
2016-01-07 10:00:44 +0800

    add the package

--- app/assets/javascripts/payment.js
+++ app/assets/javascripts/payment.js
@@ -3,3 +3,21 @@ $(document).on("click", ".open-AddPointDialog", function () {
      var myPoint = $(this).data('points');
      $(".modal-body #point_transaction_points").val( myPoint );
 });
+
+$(document).ready(function () {
+    $("#order1").shieldQRcode({
+        mode: "byte",
+        size: 150,
+        value: "https://somesite/order?plan=1"
+    });
+    $("#order2").shieldQRcode({
+        mode: "byte",
+        size: 150,
+        value: "https://somesite/order?plan=2"
+    });
+    $("#order3").shieldQRcode({
+        mode: "byte",
+        size: 150,
+        value: "https://somesite/order?plan=3"
+    });
+});
