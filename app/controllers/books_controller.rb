425976888ecb0e70d8de5b23a03df82b1b19411b
Sam <samkahchiin@gmail.com>
2016-01-08 11:17:10 +0800

    add pagination

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,8 @@
 class BooksController < ApplicationController
 
   def index
-    @books = Book.all  
+    # @books = Book.all
+    @books = Book.paginate(:page => params[:page], :per_page => 15)  
   end
 
   def new
