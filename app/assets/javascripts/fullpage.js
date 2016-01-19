91123437c6df0fcd70cd57f1721e9fa756e12f9f
Sam <samkahchiin@gmail.com>
2016-01-19 12:28:47 +0800

    overwrite the bootstrap button

--- app/assets/javascripts/fullpage.js
+++ app/assets/javascripts/fullpage.js
@@ -1,5 +1,3 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage({
-    	sectionsColor: ['white', '#D5D2CA', 'whitesmoke','#B54249', 'whitesmoke']
-    });
+    $('#fullpage').fullpage();
 });
