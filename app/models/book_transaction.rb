8cc89ff6091d4c4f0bb250ab2a5b5cdf3ab56dfa
earthworm48 <samkahchiin@gmail.com>
2016-03-12 11:25:50 +0800

    validation for book transaction

--- app/models/book_transaction.rb
+++ app/models/book_transaction.rb
@@ -1,9 +1,10 @@
 class BookTransaction < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :book
+	validates :book_id, presence: true
 
 	validate do |user|
-    	user.point_must_be_greater_than_book_price
+    	user && user.point_must_be_greater_than_book_price
   end
 
   def point_must_be_greater_than_book_price
