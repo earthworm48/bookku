917396b456ac59617d2d0a5c12fbd6e8b3b2e83b
samkahchiin <samkahchiin@gmail.com>
2016-10-15 16:42:28 +0800

    WIP - Updated users factories with trait.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,12 @@ FactoryGirl.define do
     username { Faker::Name.name }
     email { Faker::Internet.email }
     password { SecureRandom.urlsafe_base64(9) }
+
+    trait :with_books do
+      after(:create) do |user|
+        create_list :book, 2, user: user
+      end
+    end
   end
 end
 
