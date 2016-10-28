3bf1f5cb28e3b3c298bbe28cf3350dcc474dee62
samkahchiin <samkahchiin@gmail.com>
2016-10-28 22:47:04 +0800

    WIP - Included shoulda matchers in the rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,3 +10,11 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 end
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+
+    with.library :rails
+  end
+end
